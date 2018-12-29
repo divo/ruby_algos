6122b9a2d1262da301a5349be9b54879b209dc38
divo <divoapple@gmail.com>
2018-12-29 13:28:21 +0000

    Passing stack

--- stack.rb
+++ stack.rb
@@ -1,26 +1,35 @@
 class MinStack
   @stack
+  @min_stack
 
     def initialize()
       @stack = Array.new()
+      @min_stack = Array.new()
     end
 
     def push(x)
+      if @stack.last == nil || x <= @min_stack.last
+        @min_stack.push(x)
+      end
       @stack.push(x)
     end
 
     def pop()
       result = @stack.last
       @stack = @stack[0...@stack.count - 1]
+      if @min_stack.last == result
+        @min_stack.pop()
+      end
       result
     end
 
     def top()
-        
+      @stack.last
     end
 
     def get_min()
-        
+      #@min
+      @min_stack.last
     end
 
     def to_s
