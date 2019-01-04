ce992d8d025f232e8f28fb7c4b2910968d20c09e
divo <divoapple@gmail.com>
2019-01-04 19:12:40 +0000

    Linked list, not great but not bothered cleaning it atm

--- linked_list.rb
+++ linked_list.rb
@@ -7,9 +7,13 @@ class Node
     @succ = nil
   end
 
-  def to_s
-    @next = @succ.nil? ? " :END:" : succ.to_s
+  def print_list
+    @next = @succ.nil? ? " :END:" : succ.print_list
     "#{@value} -> #{@next}"
+  end
+
+  def to_s
+    "#{@value}"
   end
 
   def find(value)
@@ -23,18 +27,49 @@ class Node
   end
 
   def insert(node)
+    unless self.succ == nil
+      return self.succ.insert(node)
+    end
     old_succ = @succ
     @succ = node
     node.succ = old_succ
   end
 
+  def self.delete(head, node)
+    if head == node
+      return head.succ
+    end
+
+    if p = Node.find_prev(head, node)
+      p.succ = node.succ
+    end
+    return head
+  end
+
+  def self.find_prev(head, node)
+    # Can't have a previous || not found
+    if head == node || node == nil
+      return nil
+    end
+
+    if head.succ == node
+      return head
+    end
+
+    return Node.find_prev(head.succ, node)
+  end
 end
 
 head = Node.new(1)
-puts head.to_s
-head.insert(Node.new(2))
-puts head.to_s
-
-
-
+puts head.print_list
+two = Node.new(2)
+head.insert(two)
+puts head.print_list
+puts head.find 2
+five = Node.new(5)
+head.insert(five)
+puts head.print_list
+puts Node.find_prev(head, five)
+head = Node.delete(head, head)
+puts head.print_list
 
