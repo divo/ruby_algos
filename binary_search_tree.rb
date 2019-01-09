0cd1b6dbfc5dd40bcf2351c19f853bcdcbe3d2ec
divo <divoapple@gmail.com>
2019-01-09 21:49:17 +0000

    Not sure this insert is correct for a search tree

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,10 +1,15 @@
 class BinarySearchTree
+  # These are needed because insert is static
+  attr_reader :value
+  attr_accessor :left
+  attr_accessor :right
+
   @value
   @left
   @right
 
   def initialize(value)
-    value = value
+    @value = value
   end
 
   def search(value)
@@ -19,11 +24,42 @@ class BinarySearchTree
     end
   end
 
+  # Can I do this as an instance method?
+  def self.insert(value, node, parent)
+    if node.nil? # Base case
+      node = BinarySearchTree.new(value)
+    elsif value < parent.value
+      BinarySearchTree.insert(value, parent.left, node)
+    else
+      BinarySearchTree.insert(value, parent.right, node)
+    end
+  end
+
+  def insert(value)
+    if value < @value
+      if @left.nil?
+        @left = BinarySearchTree.new(value)
+      else
+        @left.insert(value)
+      end
+    else
+      if @right.nil?
+        @right = BinarySearchTree.new(value)
+      else
+        @right.insert(value)
+      end
+    end
+  end
+
   # In-Order traversal
   def traverse_tree
-    result = @left.nil ? "" : @left.traverse_tree
-    result += "#{value}"
-    result += @right.nil ? "" : @right.traverse_tree
+    result = @left.nil? ? "" : @left.traverse_tree
+    result += "#{@value}"
+    result += @right.nil? ? "" : @right.traverse_tree
+  end
+
+  def to_s
+    traverse_tree
   end
 
   def find_min
@@ -33,3 +69,8 @@ class BinarySearchTree
     return @value
   end
 end
+
+root = BinarySearchTree.new(5)
+puts root.traverse_tree
+BinarySearchTree.insert(4, root, root)
+puts root
