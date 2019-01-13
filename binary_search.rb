2b108bf85b87c57f4e0acce9e5aac303bd1c4043
divo <divoapple@gmail.com>
2019-01-13 16:58:01 +0000

    Proper binary search

--- binary_search.rb
+++ binary_search.rb
@@ -1,28 +1,20 @@
-# @param {Integer[]} nums
-# @param {Integer} target
-# @return {Integer}
 def search(nums, target)
   left = 0
-  right = nums.count
-  middle = find_middle(left, right)
+  right = nums.count - 1 #NOTE: This is important
 
   loop do
+    require 'byebug'; byebug
+    return -1 if left > right
+    middle = (left + right) / 2 #NOTE: THIS IS HOW TO MID POINT STOOGE
     subject = nums[middle]
     if target == subject
       return middle
-    elsif target < subject
-      right = middle
-      middle = find_middle(left, right)
+    elsif target < subject # 
+      right = middle - 1 #NOTE: THIS IS EDGES HOW ARE CHANGED
     else
-      left = middle
-      middle = find_middle(left, right)
+      left = middle + 1
     end
   end
 end
 
-def find_middle(left, right)
-  (left + right) / 2 # THIS IS HOW TO MID POINT STOOGE
-end
-
-#puts search([-1,0,3,5,9,12], 2)
-puts search([5], 5)
+puts search([-1,0,3,5,9,12], 13)
