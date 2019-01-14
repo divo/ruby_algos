47d240ab3cb9b4f3d41386720f035577bce729d8
divo <divoapple@gmail.com>
2019-01-14 08:05:16 +0000

    Start again on remove duplicates

---
+++ remove_duplicates.rb
@@ -0,0 +1,11 @@
+# Return the length of the array
+# Modify the array in place to remove duplicates
+# What's left in the array doesn't matter
+def remove_duplicates(nums)
+end
+
+
+input = [0,0,1,1,1,2,2,3,3,4]
+puts remove_duplicates(input)
+puts ""
+puts input
