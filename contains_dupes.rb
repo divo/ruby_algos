30ebc53afedfd119f71561985c282d4773523b80
Steven Diviney <divoapple@gmail.com>
2019-01-24 10:13:13 +0000

    contains_dupes with sorting and O(n) work

--- contains_dupes.rb
+++ contains_dupes.rb
@@ -7,6 +7,17 @@ def contains_duplicate(nums)
   contains_duplicate(nums)
 end
 
+# @param {Integer[]} nums
+# @return {Boolean}
+def contains_duplicate(nums)
+    return false if nums.empty?
+    nums = nums.sort
+    nums[0, nums.length - 1].each_with_index do |num, index|
+        return true if num == nums[index.next]
+    end
+    false
+end
+
 
 puts contains_duplicate([0,1,2,3,4,5])
 puts contains_duplicate([0,1,2,3,1,5])
