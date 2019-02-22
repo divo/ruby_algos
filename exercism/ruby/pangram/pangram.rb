05456c028fbd997cfb451b2248c84eecca84d111
divo <divoapple@gmail.com>
2019-02-22 15:50:15 +0000

    Pangram

---
+++ exercism/ruby/pangram/pangram.rb
@@ -0,0 +1,16 @@
+class Pangram
+  def self.regex_pangram?(string)
+    string.downcase
+      .scan(/[a-z]/)
+      .uniq
+      .count == 26
+  end
+
+  def self.pangram?(string)
+    alphabet = ('a'..'z').to_a
+    string.downcase.chars.each do |c|
+      alphabet.delete c
+    end
+    alphabet.empty?
+  end
+end
