44104620e466f2e499d53026b36101c60506007b
divo <divoapple@gmail.com>
2019-03-29 08:34:09 +0000

    More feedback on word count

--- exercism/ruby/word-count/word_count.rb
+++ exercism/ruby/word-count/word_count.rb
@@ -3,18 +3,18 @@ class Phrase
   WORD_REGEX = /\b[\w']+\b/
 
   def initialize(phrase)
-    @phrase = to_words(phrase)
+    @phrase = phrase
   end
 
   def word_count
-    @phrase.each_with_object(Hash.new(0)) do |word, result|
+    words.each_with_object(Hash.new(0)) do |word, result|
       result[word] += 1
     end
   end
 
   private
 
-  def to_words(phrase)
-    phrase.downcase.scan(WORD_REGEX)
+  def words
+    @phrase.downcase.scan(WORD_REGEX)
   end
 end
