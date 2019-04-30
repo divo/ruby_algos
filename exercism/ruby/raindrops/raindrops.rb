8044d8173b60e9264bab68dc472298e9059bbf71
divo <divoapple@gmail.com>
2019-04-30 09:17:40 +0100

    Raindrops second iteration. Looks fancy

--- exercism/ruby/raindrops/raindrops.rb
+++ exercism/ruby/raindrops/raindrops.rb
@@ -2,8 +2,17 @@
 # I was trying to find all the factors here, but I only need to look for those 3.
 # That is a fairly large error on my part
 class Raindrops
+  RAINDROPS = [[3, "Pling"], [5, "Plang"], [7, "Plong"]]
+
   def self.convert(no)
+    result = get_drops(no)
+    result == "" ? "#{no}" : result
+  end
+
+  # Convert looks a lot nicer but simple_convert is immediately understandable.
+  def self.simple_convert(no)
     result = ""
+
     if no % 3 == 0
       result += "Pling"
     end
@@ -21,9 +30,13 @@ class Raindrops
 
   private
 
-  def drop(factor, no, sound)
-    if no % factor == 0
-      sound
+  def self.get_drops(no)
+    RAINDROPS.inject("") do |result, (factor, sound)|
+      result += drop(no, factor, sound)
     end
+  end
+
+  def self.drop(no, factor, sound)
+    no % factor == 0 ? sound : ""
   end
 end
