41201aa07e92924b6869633a5d493f64b4e09f41
divo <divoapple@gmail.com>
2019-05-01 09:02:08 +0100

    Minor feedback

--- exercism/ruby/nucleotide-count/nucleotide_count.rb
+++ exercism/ruby/nucleotide-count/nucleotide_count.rb
@@ -1,5 +1,5 @@
 class Nucleotide
-  VALID_NUC = 'ACGT'.chars
+  VALID_NUCLEOTIDE = 'ACGT'.chars
 
   def self.from_dna(dna)
     Nucleotide.new(dna)
@@ -10,8 +10,8 @@ class Nucleotide
   end
 
   def histogram
-    @dna.chars.each_with_object(nucleotide_hash) do |nuc, result|
-      result[nuc] += 1
+    @dna.chars.each_with_object(nucleotide_hash) do |n, result|
+      result[n] += 1
     end
   end
 
@@ -25,11 +25,11 @@ class Nucleotide
   # Validate the dna. Could also pre-compute all the counts here
   # and use that to validate
   def validate_nucleuotides!(dna)
-    invalid = dna.chars.uniq - VALID_NUC
+    invalid = dna.chars.uniq - VALID_NUCLEOTIDE
     raise ArgumentError unless invalid.empty?
   end
 
   def nucleotide_hash
-    VALID_NUC.to_h { |k| [k, 0] }
+    VALID_NUCLEOTIDE.to_h { |k| [k, 0] }
   end
 end
