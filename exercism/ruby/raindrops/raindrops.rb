c7eac7bda78cc4780b835c7a1e7d9547960bed7c
divo <divoapple@gmail.com>
2019-05-02 09:17:19 +0100

    Feedback + cleanup raindrops

--- exercism/ruby/raindrops/raindrops.rb
+++ exercism/ruby/raindrops/raindrops.rb
@@ -2,41 +2,29 @@
 # I was trying to find all the factors here, but I only need to look for those 3.
 # That is a fairly large error on my part
 class Raindrops
-  RAINDROPS = [[3, "Pling"], [5, "Plang"], [7, "Plong"]]
+  class << self
+    RULES =
+      {
+        3 => 'Pling',
+        5 => 'Plang',
+        7 => 'Plong'
+    }
 
-  def self.convert(no)
-    result = get_drops(no)
-    result == "" ? "#{no}" : result
-  end
-
-  # Convert looks a lot nicer but simple_convert is immediately understandable.
-  def self.simple_convert(no)
-    result = ""
-
-    if no % 3 == 0
-      result += "Pling"
+    def convert(number, rules = RULES)
+      result = drops(number)
+      result.empty? ? number.to_s : result
     end
 
-    if no % 5 == 0
-      result += "Plang"
+    private
+
+    def drops(number)
+      RULES.each_with_object("") do |(factor, sound), result|
+        result << drop(number, factor, sound)
+      end
     end
 
-    if no % 7 == 0
-      result += "Plong"
+    def drop(number, factor, sound)
+      number % factor == 0 ? sound : ""
     end
-
-    result == "" ? "#{no}" : result
-  end
-
-  private
-
-  def self.get_drops(no)
-    RAINDROPS.inject("") do |result, (factor, sound)|
-      result += drop(no, factor, sound)
-    end
-  end
-
-  def self.drop(no, factor, sound)
-    no % factor == 0 ? sound : ""
   end
 end
