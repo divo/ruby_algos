a18dd8086dc8630a43dcc27df53e5545e892727e
divo <divoapple@gmail.com>
2019-05-06 15:48:29 +0100

    Raindrops again
    Introducing a load of state this time. This has to be worse?

--- exercism/ruby/raindrops/raindrops.rb
+++ exercism/ruby/raindrops/raindrops.rb
@@ -1,30 +1,23 @@
-# Output strings for 3, 5, 7.
-# I was trying to find all the factors here, but I only need to look for those 3.
-# That is a fairly large error on my part
 class Raindrops
-  class << self
-    RULES =
-      {
-        3 => 'Pling',
-        5 => 'Plang',
-        7 => 'Plong'
-      }
+  RULES =
+    {
+      3 => 'Pling',
+      5 => 'Plang',
+      7 => 'Plong'
+  }
 
-    def convert(number, rules = RULES)
-      result = drops(number, rules)
-      result.empty? ? number.to_s : result
+  def self.convert(number)
+    new(number).convert
+  end
+
+  def initialize(number)
+    @number = number
+  end
+
+  def convert
+    result = RULES.each_with_object('') do |(factor, sound), result|
+      result << sound if (@number % factor).zero?
     end
-
-    private
-
-    def drops(number, rules)
-      rules.each_with_object('') do |(factor, sound), result|
-        result << drop(number, factor, sound)
-      end
-    end
-
-    def drop(number, factor, sound)
-      (number % factor).zero? ? sound : ''
-    end
+    result.empty? ? @number.to_s : result
   end
 end
