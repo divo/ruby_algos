49bcef3ccc5e44a0dfa5a2e3c904789bfa1b6864
divo <divoapple@gmail.com>
2019-05-15 09:11:08 +0100

    Minor feedback

--- exercism/ruby/isogram/isogram.rb
+++ exercism/ruby/isogram/isogram.rb
@@ -1,6 +1,6 @@
 module Isogram
   def self.isogram?(string)
-    stripped_string = string.downcase.gsub(/[^a-z0-9]/i, '')
-    stripped_string.length == stripped_string.chars.uniq.count
+    stripped_string = string.downcase.gsub(/[^a-z]/i, '')
+    stripped_string.length == stripped_string.chars.uniq.length
   end
 end
