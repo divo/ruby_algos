775f4c8aa5a718b8bd6cdceaac96b9983643d23e
divo <divoapple@gmail.com>
2019-05-30 21:22:28 +0100

    Proverb

--- exercism/ruby/proverb/proverb.rb
+++ exercism/ruby/proverb/proverb.rb
@@ -1,7 +1,8 @@
 class Proverb
 
-  def initialize(*list)
+  def initialize(*list, qualifier: "")
     @list = list
+    @qualifier = qualifier
   end
 
   def to_s
@@ -10,14 +11,15 @@ class Proverb
     @list[1, @list.count].each_index do |i|
       result << template(@list[i], @list[i + 1])
     end
-    result << end_template(@list.first)
+    result << end_template(@list.first, @qualifier)
   end
 
   def template(first, second)
     "For want of a #{first} the #{second} was lost.\n"
   end
 
-  def end_template(first)
-    "And all for the want of a #{first}."
+  def end_template(last, qualifier)
+    qualifier += " " unless qualifier.empty?
+    "And all for the want of a #{qualifier}#{last}."
   end
 end
--- exercism/ruby/proverb/proverb_test.rb
+++ exercism/ruby/proverb/proverb_test.rb
@@ -10,7 +10,7 @@ class ProverbTest < Minitest::Test
   end
 
   def test_a_short_chain_of_consequences
-    skip
+    
     proverb = Proverb.new('nail', 'shoe', 'horse')
     expected = "For want of a nail the shoe was lost.\n" \
       "For want of a shoe the horse was lost.\n" \
@@ -28,7 +28,7 @@ class ProverbTest < Minitest::Test
   end
 
   def test_proverb_does_not_hard_code_the_rhyme_dictionary
-    skip
+    
     proverb = Proverb.new('key', 'value')
     expected = "For want of a key the value was lost.\n" \
       'And all for the want of a key.'
@@ -36,7 +36,7 @@ class ProverbTest < Minitest::Test
   end
 
   def test_the_whole_proverb
-    skip
+    
     chain = %w(nail shoe horse rider message battle kingdom)
     proverb = Proverb.new(*chain)
     expected = "For want of a nail the shoe was lost.\n" \
@@ -50,7 +50,7 @@ class ProverbTest < Minitest::Test
   end
 
   def test_an_optional_qualifier_in_the_final_consequence
-    skip
+    
     chain = %w(nail shoe horse rider message battle kingdom)
     proverb = Proverb.new(*chain, qualifier: 'horseshoe')
     expected = "For want of a nail the shoe was lost.\n" \
@@ -64,7 +64,7 @@ class ProverbTest < Minitest::Test
   end
 
   def test_proverb_is_same_each_time
-    skip
+    
     proverb = Proverb.new('nail', 'shoe')
     assert_equal proverb.to_s, proverb.to_s
   end
