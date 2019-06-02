425fc49325f79c1e4caaf0416afab279686ec6b6
divo <divoapple@gmail.com>
2019-06-02 07:04:51 +0100

    Enumerable example

--- enumerable.rb
+++ enumerable.rb
@@ -1,1 +1,17 @@
-# TODO: Enumerable Docs, (And Enumerator?)
+# To implement enumerable for a class, simply provide an each that
+# yields successive members of the collection. You must also provide <=>
+# if using #min #max #sort
+
+class EnumLike
+  include Enumerable
+  def each(&block)
+    (0..10).each(&block)
+  end
+
+  def each_alternative
+    (0..10).each { |x| yield x }
+  end
+end
+
+enum = EnumLike.new()
+enum.each { |x| puts x }
