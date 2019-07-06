8b1519f25d2b41f22da60942c99cad9e9e4a28a9
Steven Diviney <divoapple@gmail.com>
2019-07-06 19:50:38 +0000

    More feedback

--- exercism/ruby/scrabble-score/scrabble_score.rb
+++ exercism/ruby/scrabble-score/scrabble_score.rb
@@ -1,27 +1,30 @@
 class Scrabble
 
-  POINTS  = {  "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-               "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-               "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-               "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-               "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-               "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-               "Y"=>4, "Z"=>10
+  LETTER_TO_SCORE  = {
+    "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+    "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+    "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+    "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+    "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+    "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+    "Y"=>4, "Z"=>10
   }
-  POINTS.default = 0
+  LETTER_TO_SCORE.default = 0
+  LETTER_TO_SCORE.freeze
 
+  attr_reader :word
   def initialize(word)
     @word = word
+    @score = score
   end
 
   def score
     return 0 unless @word
-
-    tiles(@word).sum { |char| POINTS[char] }
+    tiles.sum(&LETTER_TO_SCORE)
   end
 
-  def tiles(word)
-    word.upcase.chars
+  def tiles
+    @word.upcase.chars
   end
 
   def self.score(word)
