8a05d06dd96bb2cd3a764d96f9f3b8c1432b604e
Steven Diviney <divoapple@gmail.com>
2019-07-07 09:06:00 +0000

    Layout for work

--- exercism/ruby/luhn/luhn.rb
+++ exercism/ruby/luhn/luhn.rb
@@ -1,5 +1,24 @@
 class Luhn
   def valid(id)
-    return true
+    # Algo:
+  end
+
+  # Must have length > 1
+  # Spaces are allowed but must be stripped
+  # All other non number chars are not allowed
+  def validate_input(id)
+    return false if id.length <= 1
+  end
+
+  # Double every second digit
+  # If doubling results in no > 9, then subtract 9 from the product
+  # Sum all the digits, it's divisible by 10 then it's valid
+  def is_luhn?(id)
+
+  end
+
+  # TODO: Better name for this
+  def double_in_place(id)
+
   end
 end
