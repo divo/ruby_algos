8a87686795f182d1f3506925820c2b1a7f7b6d01
Steven Diviney <divoapple@gmail.com>
2019-07-08 07:35:48 +0000

    First draft of answer, made a big mess
    Seems to be a problem with the algo implementation :(

--- exercism/ruby/luhn/luhn.rb
+++ exercism/ruby/luhn/luhn.rb
@@ -1,9 +1,8 @@
 class Luhn
   class << self
     def valid?(id)
-      # Algo:
       return false if (id = validate_input(id)).nil?
-      id
+      is_luhn?(id)
     end
 
     # Must have length > 1
@@ -11,7 +10,7 @@ class Luhn
     # All other non number chars are not allowed
     def validate_input(id)
       id = id.delete(' ')
-      return nil if id.to_i.to_s != id
+      return nil if id.match(/^(\d)+$/).nil?
       return nil if id.length <= 1
       id
     end
@@ -20,12 +19,24 @@ class Luhn
     # If doubling results in no > 9, then subtract 9 from the product
     # Sum all the digits, it's divisible by 10 then it's valid
     def is_luhn?(id)
+      digits = id.chars.map(&:to_i)
+      result = []
+      digits.each_slice(2) do |first, second|
+        result << first
+        result << double_in_place(second)
+      end
 
+      result.compact.sum % 10 == 0
     end
 
     # TODO: Better name for this
-    def double_in_place(id)
-
+    def double_in_place(number)
+      return nil if number.nil? # Handle odd number of digits
+      result = number * 2
+      if result > 9
+        result = result - 9
+      end
+      result
     end
   end
 end
