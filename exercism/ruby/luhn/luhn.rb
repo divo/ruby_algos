9551d68271b7e349d1df0eca31c540dba6fe755b
Steven Diviney <divoapple@gmail.com>
2019-07-09 09:03:53 +0000

    Cleaning up
    Just need to think of a name for the algo body

--- exercism/ruby/luhn/luhn.rb
+++ exercism/ruby/luhn/luhn.rb
@@ -5,31 +5,35 @@ class Luhn
       is_luhn?(id)
     end
 
-    # Must have length > 1
-    # Spaces are allowed but must be stripped
-    # All other non number chars are not allowed
     def validate_input(id)
-      id = id.delete(' ')
-      return nil if id.match(/^(\d)+$/).nil?
-      return nil if id.length <= 1
-      id
+      id.delete!(' ')
+      if id.match(/^(\d)+$/).nil? || id.length <= 1
+        nil
+      else
+        id
+      end
     end
 
-    # Double every second digit
-    # If doubling results in no > 9, then subtract 9 from the product
-    # Sum all the digits, it's divisible by 10 then it's valid
     def is_luhn?(id)
-      digits = id.chars.map(&:to_i)
+      digits = to_i_a(id)
+      result = thing(digits)
+      result.compact.sum % 10 == 0
+    end
+
+    def to_i_a(string)
+      string.chars.map(&:to_i)
+    end
+
+    def thing(digits)
       result = []
       digits.reverse.each_slice(2) do |first, second|
         result << first
         result << double_in_place(second)
       end
 
-      result.compact.sum % 10 == 0
+      result
     end
 
-    # TODO: Better name for this
     def double_in_place(number)
       return nil if number.nil? # Handle odd number of digits
       result = number * 2
