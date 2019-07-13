4438e9f3a2df2ab68a79fd6398f350e9e03148b8
divo <divoapple@gmail.com>
2019-07-13 22:02:23 +0100

    Encode actaully working now

--- exercism/ruby/run-length-encoding/run_length_encoding.rb
+++ exercism/ruby/run-length-encoding/run_length_encoding.rb
@@ -1,13 +1,25 @@
+# TODO: Make this a module
 class RunLengthEncoding
   class << self
     def encode(string)
       return '' if string.empty?
+
       groups = string.chars.chunk_while { |x, y| x == y }
       result = groups.map do |group|
         length = group.length > 1 ? group.length : nil
         "#{length}#{group.first}"
       end
       result.join
+    end
+
+    def decode(string)
+      # Chunk into nos and chars, then each_cons it
+      chunks = string.chars.chunk { |x| number?(x) }
+      require 'byebug'; byebug;
+    end
+
+    def number?(x)
+      x == x.to_i.to_s
     end
   end
 end
