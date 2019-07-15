905a53b02fd3040221a4b82314e346da5b0a6c95
divo <divoapple@gmail.com>
2019-07-15 20:55:56 +0100

    First go at mentor feedback. I don't get it -_-

--- exercism/ruby/luhn/luhn.rb
+++ exercism/ruby/luhn/luhn.rb
@@ -2,40 +2,51 @@
 
 # Luhn checks if a string is a valid Luhn
 class Luhn
-  class << self
-    def valid?(id)
-      id.delete!(' ')
-      return false unless valid_input?(id)
+  def self.valid?(candidate)
+    new(candidate).valid?
+  end
 
-      digits = to_i_a(id)
-      digits = double_every_second(digits)
-      luhn?(digits)
-    end
+  # Rubys love of instance state is a bit confusing to me
+  def initialize(candidate)
+    @candidate = candidate.delete(' ')
+  end
 
-    def valid_input?(id)
-      !!id.match(/^(\d)+$/) && id.length > 1
-    end
+  def valid?
+    return false unless valid_input?
 
-    def to_i_a(string)
-      string.chars.map(&:to_i)
-    end
-
-    def double_every_second(digits)
-      digits.reverse.each_slice(2).flat_map do |first, second|
-        [first, double_in_place(second)]
+    @valid ||=
+      begin
+        digits = convert(@candidate)
+        digits = double_every_second(digits)
+        luhn?(digits)
       end
-    end
+  end
 
-    def double_in_place(number)
-      return nil unless number # Handle odd number of digits
+  private
 
-      result = number * 2
-      result -= 9 if result > 9
-      result
-    end
+  def valid_input?
+    !!@candidate.match(/^(\d)+$/) && @candidate.length > 1
+  end
 
-    def luhn?(digits)
-      (digits.compact.sum % 10).zero?
+  def convert(string)
+    string.chars.map(&:to_i)
+  end
+
+  def double_every_second(digits)
+    digits.reverse.each_slice(2).flat_map do |first, second|
+      [first, double_in_place(second)]
     end
+  end
+
+  def double_in_place(number)
+    return nil unless number # Handle odd number of digits
+
+    result = number * 2
+    result -= 9 if result > 9
+    result
+  end
+
+  def luhn?(digits)
+    (digits.compact.sum % 10).zero?
   end
 end
