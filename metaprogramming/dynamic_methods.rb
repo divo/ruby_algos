bc280c314484ea6f1e7a0301113070decbd267ed
divo <divoapple@gmail.com>
2019-07-20 20:59:11 +0100

    Multiple dynmaic methods

--- metaprogramming/dynamic_methods.rb
+++ metaprogramming/dynamic_methods.rb
@@ -3,7 +3,16 @@ class Test
   define_method :one do |arg|
     arg * 10
   end
+
+  METHODS = [:two, :three]
+  METHODS.map do |method|
+    define_method method do |arg|
+      "#{arg} #{method}"
+    end
+  end
 end
 
 
 puts Test.new.one 10
+puts Test.new.two 20
+puts Test.new.three 30
