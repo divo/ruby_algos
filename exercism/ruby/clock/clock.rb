8c5c960284e13db1f8e3a790b9af2983a5ccc831
divo <divoapple@gmail.com>
2019-08-31 22:08:54 +0100

    Have clock output working

--- exercism/ruby/clock/clock.rb
+++ exercism/ruby/clock/clock.rb
@@ -1,13 +1,39 @@
 class Clock
-  def initialize(hour: 0, minutes: 0)
+
+  def initialize(hour: 0, minute: 0)
     @hour = hour
-    @minutes = minutes
+    @minute = minute
   end
 
   def to_s
-    hour = @hour < 10 ? "0#{@hour}" : "#{@hour}"
-    minutes = @minutes < 10 ? "0#{@minutes}" : "#{@minutes}"
+    minute = format_minute(@minute)
+    "#{format_hour(@hour)}:#{minute}"
+  end
 
-    "#{hour}:#{minutes}"
+  private
+
+  def format_hour(hour)
+    case hour
+    when 0..10
+      "0#{hour}"
+    when 11..23
+      hour.to_s
+    else
+      format_hour(hour % 24)
+    end
+  end
+
+  def format_minute(minute)
+    case minute
+    when 0..10
+      "0#{minute}"
+    when 11..59
+      minute.to_s
+    else
+      # Division will provide the number of hours
+      @hour += (minute / 60).floor
+      # Mod will provide the number of minutes
+      format_minute(minute % 60)
+    end
   end
 end
