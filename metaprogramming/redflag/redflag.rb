6907eb92a22b548f1cff883935016ee6257fa90d
divo <divoapple@gmail.com>
2019-11-19 09:06:11 -0800

    RedFlag, a basic testing framework
    Added clean room to event execution, now events
    can no longer mutate the env on eachother

--- metaprogramming/redflag/redflag.rb
+++ metaprogramming/redflag/redflag.rb
@@ -2,18 +2,23 @@ def event(description, &block)
   @events << { description: description, condition: block }
 end
 
+# There are now basically globals that get set for every event.
+# The book provides a solution to create a shared scope but it's ugly
 def setup(&block)
-  @env << block
+  @state << block
 end
 
 def run
   @events.each do |event|
-    @env.each(&:call)
-    puts event[:description] if event[:condition].call
+    env = Object.new
+    @state.each do |s|
+      env.instance_eval(&s)
+    end
+    puts event[:description] if env.instance_eval(&event[:condition])
   end
 end
 
 @events = []
-@env = []
+@state = []
 load 'events.rb'
 run
