ca0f2399f27810caa67bcb934ad04f4b686c71e6
divo <divoapple@gmail.com>
2019-11-19 08:49:38 -0800

    Basic v2 working, but I still need to store the scope

--- metaprogramming/redflag/events.rb
+++ metaprogramming/redflag/events.rb
@@ -1,7 +1,21 @@
-event 'an event that is always true' do
-  true
+setup do
+  puts 'setting up sky'
+  @sky_height = 100
 end
 
-event 'an event that is always false' do
-  false
+setup do
+  puts 'setting up mountains'
+  @mountains_height = 200
+end
+
+event 'the sky is falling' do
+  @sky_height < 300
+end
+
+event 'its getting closer' do
+  @sky_height < @mountains_height
+end
+
+event 'too late' do
+  @sky_height < 0
 end
--- metaprogramming/redflag/redflag.rb
+++ metaprogramming/redflag/redflag.rb
@@ -1,4 +1,19 @@
-def event(description)
-  puts "ALERT: #{description}" if yield
+def event(description, &block)
+  @events << { description: description, condition: block }
 end
+
+def setup(&block)
+  @env << block
+end
+
+def run
+  @events.each do |event|
+    @env.each(&:call)
+    puts event[:description] if event[:condition].call
+  end
+end
+
+@events = []
+@env = []
 load 'events.rb'
+run
