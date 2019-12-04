5bf394f2f77847c926c2ba241a3a6109e74dee3f
divo <divoapple@gmail.com>
2019-12-04 08:44:43 -0800

    Using refinements to Alias

--- metaprogramming/alias.rb
+++ metaprogramming/alias.rb
@@ -8,3 +8,16 @@ end
 
 puts "Testing string".real_length
 puts "Testing string".length
+
+module StringRefinment
+  refine String do
+    def size
+      super > 5 ? 'many' : 'few'
+    end
+  end
+end
+
+
+puts "Testing string".size
+using StringRefinment
+puts "Testing string".size
