0074a9bce8d9a7372d9840c43146dc7ff97e6917
divo <divoapple@gmail.com>
2019-12-10 08:48:34 -0800

    Adding checked_attr to Class

--- metaprogramming/check_attr/checked_attr_2.rb
+++ metaprogramming/check_attr/checked_attr_2.rb
@@ -1,7 +1,6 @@
 # The one without eval
-# Part 2
-def add_checked_attribute(klass, attribute, &validation)
-  klass.class_eval do
+class Class
+  def attr_checked(attribute, &validation)
     define_method "#{attribute}=" do |value|
       raise unless validation.call(value)
       instance_variable_set("@#{attribute}", value)
@@ -13,9 +12,12 @@ def add_checked_attribute(klass, attribute, &validation)
   end
 end
 
-class Test; end
+class Test
+  attr_checked :x do |x|
+    x <= 6
+  end
+end
 
-add_checked_attribute(Test, :x) { |x| x <= 6 }
 test = Test.new
 test.x = 3
 test.x = 10
