8992210d4f80d465a6cb751fd69950a4d03c6aab
divo <divoapple@gmail.com>
2023-10-25 15:22:32 +0100

    Starting on interview prep, two sum

---
+++ interview_prep/README.md
@@ -0,0 +1,9 @@
+# Interview prep
+Notes for different interview questions. Data structure etc notes are in Obsiden
+
+## two_sum.rb
+Naive solution is N^2 and pretty easy to figure out. Only trick regarding `each_with_index` is the inner loop index will be relative so you need to add the outher loop index to it.
+
+O(n) solution builds a hash of the input and then iterates the numbers looking for the current compliment in the hash.
+
+I gather the "real" interview way is to use two pointers coming from each end of the array. If the result is lower than target, increment the right pointer, if the result is higher decrement the left pointer.
---
+++ interview_prep/two_sum.rb
@@ -0,0 +1,31 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    map = {}
+    for i in 0..nums.length do
+        map[nums[i]] = i
+    end
+
+    for i in 0..nums.length do
+        complement = target - nums[i]
+        if map.include?(complement) && map[complement] != i
+            return [i, map[complement]]
+        end
+    end
+end
+
+def two_sum_brute_force(nums, target)
+    l_index, r_index = 0, 0
+    nums.each_with_index do |r_val, idx| 
+        l_index = idx
+        nums[idx + 1..nums.length].each_with_index do |l_val, inner_idx|
+            r_index = inner_idx + idx + 1 # each_with_index means I have to add prior index
+            if r_val + l_val == target
+                return [l_index, r_index]
+            elsif l_val > target
+                next
+            end
+        end
+    end
+end
