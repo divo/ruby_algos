52524fdca66b880acc1101c6042817a735ebc090
divo <divoapple@gmail.com>
2023-10-27 09:46:46 +0100

    Palindrome two pointers

--- interview_prep/code/125.valid-palindrome.rb
+++ interview_prep/code/125.valid-palindrome.rb
@@ -1,6 +1,20 @@
 # @param {String} s
 # @return {Boolean}
 def is_palindrome(s)
+  chars = s.downcase.gsub(/[[:punct:]\s]/, '')
+  left, right = 0, chars.length - 1
+
+  while left < right
+    return false unless chars[left] == chars[right]
+
+    left += 1
+    right -= 1
+  end
+
+  true
+end
+
+def is_palindrome_arrays(s)
   letters = s.downcase.gsub(/[[:punct:]\s]/, '').chars
   middle = letters.count / 2
   end_idx = letters.count
