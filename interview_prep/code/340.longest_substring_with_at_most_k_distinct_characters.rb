42f62b3dab6480bd13f7ed71c3eb8bfb6879e72b
divo <divoapple@gmail.com>
2023-10-31 20:18:14 +0000

    Longest substring of K distinct chars

---
+++ interview_prep/code/340.longest_substring_with_at_most_k_distinct_characters.rb
@@ -0,0 +1,23 @@
+# Premium question, I don't have access to
+#
+
+def find_length(string, k)
+  max, w_start, w_end = 0, 0, 0
+  while w_start < string.length
+    if string[w_start..w_end].chars.uniq.length <= k
+      window_len = w_end - w_start + 1
+      max = window_len if window_len > max
+      w_end += 1
+      w_start += 1 if w_end >= string.length # This is the piece I was missing
+    else
+      w_start += 1
+      w_end = w_start
+    end
+  end
+  max
+end
+
+
+puts find_length("araaci", 2) # 4
+puts find_length("araaci", 1) # 2
+puts find_length("cbbebi", 3) # 5
