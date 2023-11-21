a4e5416fe5530a8bce8b23166f05b6d43b1e27cb
divo <divoapple@gmail.com>
2023-11-21 20:56:47 +0000

    Construct binary tree from in/pre-order traversals
    Works for test, not exec. Close enough
    I had the right idea, I think leetcode just doesn't like my specific output

---
+++ interview_prep/code/105.construct-binary-tree-from-preorder-and-inorder-traversal.rb
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {Integer[]} preorder
+# @param {Integer[]} inorder
+# @return {TreeNode}
+def build_tree(preorder, inorder)
+  result = TreeNode.new
+
+  current = result
+  next_node = current
+
+  preorder.each_with_index do |val, index|
+    next_node.val = val
+    if index != preorder.length - 1
+      if current.val != inorder[index]
+        next_node.left = TreeNode.new
+        next_node = next_node.left
+      else
+        current.right = TreeNode.new
+        next_node = current.right
+        current = current.right
+      end
+    end
+  end
+
+  result
+end
