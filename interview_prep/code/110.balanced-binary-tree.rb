270cf4dbbde0a007dd435099b70ef2214be898e9
divo <divoapple@gmail.com>
2023-12-03 18:30:55 +0000

    Simple fix for balanced binary tree

--- interview_prep/code/110.balanced-binary-tree.rb
+++ interview_prep/code/110.balanced-binary-tree.rb
@@ -10,15 +10,18 @@
 # @param {TreeNode} root
 # @return {Boolean}
 def is_balanced(root)
+  return true unless root
+    
   queue = [[root, 1]]
   max = nil
   until queue.empty?
     node, depth = queue.shift
-    max = depth unless node
+    max ||= depth unless node&.left && node&.right
+
     return false if max && depth > max + 1 # Allowed a difference of 1
 
-    queue << [node&.left, depth + 1]
-    queue << [node&.right, depth + 1]
+    queue << [node.left, depth + 1] if node.left
+    queue << [node.right, depth + 1] if node.right
   end
 
   true
