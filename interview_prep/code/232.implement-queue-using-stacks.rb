96d51f1992e0e806eb2781c992887c29c44e27c5
divo <divoapple@gmail.com>
2023-12-04 18:10:00 +0000

    232, queue using stacks. Right idea, a bit too inefficient

---
+++ interview_prep/code/232.implement-queue-using-stacks.rb
@@ -0,0 +1,35 @@
+class MyQueue
+  def initialize()
+    @queue = []
+    @buffer = []
+  end
+
+  def push(x)
+    @queue << x
+  end
+
+  def pop
+    @buffer << @queue.pop until @queue.empty? 
+    result = @buffer.pop
+    @queue << @buffer.pop until @buffer.empty?
+    result
+  end
+
+  def peek()
+    @buffer << @queue.pop until @queue.empty? 
+    result = @buffer.last # last == peek
+    @queue << @buffer.pop until @buffer.empty?
+    result
+  end
+
+  def empty()
+    @queue.empty?
+  end
+end
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue.new()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
