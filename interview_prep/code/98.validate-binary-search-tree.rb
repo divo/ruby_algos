314ced429294b074e8a408e926baa0e4beb4a612
divo <divoapple@gmail.com>
2023-12-22 20:00:14 +0000

    Redo valid BST, very easy when you know how

--- interview_prep/code/98.validate-binary-search-tree.rb
+++ interview_prep/code/98.validate-binary-search-tree.rb
@@ -1,25 +1,20 @@
 # Definition for a binary tree node.
 # class TreeNode
-#     attr_accessor :val, :left, :right
-#     def initialize(val = 0, left = nil, right = nil)
-#         @val = val
-#         @left = left
-#         @right = right
-#     end
+#   attr_accessor :val, :left, :right
+#   def initialize(val = 0, left = nil, right = nil)
+#     @val = val
+#     @left = left
+#     @right = right
+#   end
 # end
 # @param {TreeNode} root
 # @return {Boolean}
 def is_valid_bst(root, min = nil, max = nil)
   return true if root.nil?
 
-end
+  return false if root.val <= min if min
+  return false if root.val >= max if max
 
-def is_valid_bst(node, left = nil, right = nil)
-  return true if node.nil?
-
-  return false if left && node.val <= left
-  return false if right && node.val >= right
-
-  is_valid_bst(node.left, left, node.val) &&
-    is_valid_bst(node.right, node.val, right)
+  is_valid_bst(root.left, min, root.val) &&
+    is_valid_bst(root.right, root.val, max)
 end
