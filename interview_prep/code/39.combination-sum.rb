4e5bff8a5430be12af1346862a1f226d5a4ac194
divo <divoapple@gmail.com>
2024-01-10 12:00:45 +0000

    Combination sum with a LOT of help

--- interview_prep/code/39.combination-sum.rb
+++ interview_prep/code/39.combination-sum.rb
@@ -2,6 +2,31 @@
 # @param {Integer} target
 # @return {Integer[][]}
 def combination_sum(candidates, target)
+  @result = [] # ANKI: Nested functions can only access instance variables
+  @target = target
+  @candidates = candidates
+
+  def dfs(current, k)
+    return if k >= @candidates.size
+
+    return if current.sum > @target
+
+    if current.sum == @target
+      @result << current.dup # Would not have got this
+      return
+    end
+
+    current << @candidates[k]
+    dfs(current, k)
+    current.pop # A little wasteful but easier to reason about
+    dfs(current, k + 1)
+  end
+
+  dfs([], 0)
+  @result
+end
+
+def combination_sum_bad(candidates, target)
   result = []
   10.times do |n|
     result += candidates
@@ -12,3 +37,5 @@ def combination_sum(candidates, target)
   end
   result.compact.reject(&:empty?)
 end
+
+# puts combination_sum([2, 3, 5], 8)
