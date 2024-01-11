c86afee03de2b090046d0167f5a5f49e87b66e72
divo <divoapple@gmail.com>
2024-01-11 21:17:49 +0000

    46. Permutations finaly got it

--- interview_prep/code/46.permutations.rb
+++ interview_prep/code/46.permutations.rb
@@ -1,5 +1,28 @@
 # @param {Integer[]} nums
 # @return {Integer[][]}
 def permute(nums)
+  result = []
+  build([], nums, result)
+  result
+end
+
+def build(current, nums, result)
+  buffer = current.dup
+  if nums.empty?
+    result << current.dup
+    return
+  end
+
+  nums.each do |head|
+    buffer << head
+    tail = nums.dup
+    tail.delete(head)
+    build(buffer, tail, result)
+    buffer.pop
+  end
+end
+
+def permute_simple(nums)
   nums.permutation(nums.length).to_a
 end
+
