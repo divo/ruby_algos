6a12a1ef47d09cf59da23e4c18871ddc7f82aa39
divo <divoapple@gmail.com>
2024-03-24 17:44:46 +0000

    287 find duplicate with sort

--- interview_prep/code/287.find-the-duplicate-number.rb
+++ interview_prep/code/287.find-the-duplicate-number.rb
@@ -1,6 +1,15 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_duplicate(nums)
+  last = nil
+  nums.sort.each do |n|
+    return n if n == last
+
+    last = n
+  end
+end
+
+def find_duplicate_variable_mem(nums)
   seen = Array.new(nums.length)
   nums.each do |n|
     return n if seen[n]
