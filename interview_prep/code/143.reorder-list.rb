fa7c8c7107b6674a409e27eac0de93a031f41696
--show-origin <divoapple@gmail.com>
2024-06-18 10:56:03 +0100

    143 Reorder list redo, bit neater this time. No trouble

--- interview_prep/code/143.reorder-list.rb
+++ interview_prep/code/143.reorder-list.rb
@@ -6,27 +6,32 @@ class ListNode
         @next = _next
     end
 end
+# [1, 5, 2, 4, 3]
 # @param {ListNode} head
 # @return {Void} Do not return anything, modify head in-place instead.
 def reorder_list(head)
-  list, r_list = [], [] # I don't need two lists! I can just build one and then pop and shift from the same list
+  stack = list_to_stack(head)
+  stack = stack.reverse[0...stack.length / 2]
   current = head
-  until current.nil?
-    list.push current
-    r_list.unshift current
-    current = current.next
+  until stack.empty?
+    nxt = current.next
+    tail = stack.shift
+    current.next = tail
+    tail.next = nxt
+    current = nxt
   end
 
-  head = list.shift
-  current = head
-  (1..list.length).each do |i|
-    if i.even?
-      current.next = list.shift
-      current = current.next
-    else
-      current.next = r_list.shift
-      current = current.next
-    end
-  end
   current.next = nil
+  head
 end
+
+def list_to_stack(head)
+  result = []
+  until head.nil?
+    result << head
+    head = head.next
+  end
+  result
+end
+
+# ll = ListNode.new(1, ListNode.new(2, ListNode.new(3, ListNode.new(4, ListNode.new(5)))))
