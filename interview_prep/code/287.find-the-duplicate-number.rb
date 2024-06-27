ab37387ade1e0ecddc925263cb344cc258adbf69
--show-origin <divoapple@gmail.com>
2024-06-27 13:18:08 +0100

    287. Find duplicate. Almost works but my approach is fundamentailly flawed....

--- interview_prep/code/287.find-the-duplicate-number.rb
+++ interview_prep/code/287.find-the-duplicate-number.rb
@@ -1,26 +1,31 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_duplicate(nums)
-  nums.each_with_index do |val, idx|
-    return val unless nums[idx + 1..].select { |x| x == val }.empty?
+  # Use a fast and slow pointer, return when they meet
+  return nums.first if nums.length <= 2
+
+  slow, fast = 0, 1
+  loop do
+    return nums[slow] if nums[slow] == nums[fast]
+    slow += 1
+    fast += 2
+
+    slow = 0 if slow >= nums.length
+    fast = 1 if fast >= nums.length
   end
 end
 
-def find_duplicate_sort_extra_mem(nums)
-  last = nil
-  # This is using extra space though!
-  nums.sort.each do |n|
-    return n if n == last
-
-    last = n
+def find_duplicate_brute_force(nums)
+  nums.each_with_index do |n, i|
+    nums[(i + 1)..].each do |j|
+      if j == n
+        return n
+      end
+    end
   end
+  return -1
 end
 
-def find_duplicate_variable_mem(nums)
-  seen = Array.new(nums.length)
-  nums.each do |n|
-    return n if seen[n]
-
-    seen[n] = n
-  end
-end
+# How to reset fast and slow so they don't meet?
+find_duplicate([3,1,3,4,2])
+find_duplicate([4,3,1,4,2])
