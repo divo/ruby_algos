15ff2a27a5791c19228ef00e43aa2cf3b7088e57
--show-origin <divoapple@gmail.com>
2024-07-02 12:16:02 +0100

    23. Merge K-sorted lists, fail
    Used brute force, works for test but timesout on long input.
    Working solution requires a priority queue or a min heap.
    Stopping here as my approach wont work and I need to
    study the structures needed to solve this
    Not a bad effort though, did have to introduce some shitty code
    to handle bad input

---
+++ interview_prep/code/23.merge-k-sorted-lists.rb
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode[]} lists
+# @return {ListNode}
+def merge_k_lists(lists)
+  return nil if lists.empty? || (lists.first&.val == nil && lists.length == 1)
+
+  return lists.first if lists.length == 1
+
+  current = result = ListNode.new
+  until lists.empty?
+    min = [Integer::MAX, -1] # Store tuple if node and index
+    lists.each_with_index do |node, index|
+      min = [[node&.val || Integer::MAX, index], min].min_by(&:first)
+    end
+    min_val, min_index = min
+    if min_val != Integer::MAX # Handle not finding a min, when we have junk input
+      current.next = ListNode.new(min_val)
+      current = current.next
+    end
+    lists[min_index] = lists[min_index]&.next
+    lists = lists.compact
+  end
+  result.next
+end
+
+
+class Integer
+  N_BYTES = [42].pack('i').size
+  N_BITS = N_BYTES * 16
+  MAX = 2 ** (N_BITS - 2) - 1
+  MIN = -MAX - 1
+end
