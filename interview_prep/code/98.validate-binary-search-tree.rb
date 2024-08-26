cb570c37756fe01fb76b1eff57f942352d1ecd68
--show-origin <divoapple@gmail.com>
2024-08-26 11:08:54 +0100

    98, clean up a little
    Improve readability

--- interview_prep/code/98.validate-binary-search-tree.rb
+++ interview_prep/code/98.validate-binary-search-tree.rb
@@ -12,8 +12,8 @@
 def is_valid_bst(root, min = nil, max = nil)
   return true if root.nil?
 
-  return false if root.val <= min if min
-  return false if root.val >= max if max
+  return false if min && root.val <= (min)
+  return false if max && root.val >= (max)
 
   is_valid_bst(root.left, min, root.val) &&
     is_valid_bst(root.right, root.val, max)
