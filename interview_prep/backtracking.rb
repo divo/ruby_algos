6adc5efd0ec2437a0410d92cbcb7baa1a0a36b6b
--show-origin <divoapple@gmail.com>
2024-08-27 11:49:33 +0100

    Clean up backtracking jsut a bit

--- interview_prep/backtracking.rb
+++ interview_prep/backtracking.rb
@@ -3,31 +3,32 @@
 # Assume this is non-functional pseudo code
 
 # Generic backtrack solution
-# def backtrack_from_book(partial, input, result, position = 0)
-#   if a_solution?(partial, position, input)
-#     result << process_solution(partial, input)
+# def backtrack_from_book(subset, input, result, position = 0)
+#   if a_solution?(subset, position, input)
+#     result << process_solution(subset, input)
 #   else
 #     candidates = construct_candidates
 #     candidates.each do |candidate|
-#       partial[position] = candidate
-#       backtrack(partial, input, result, position + 1)
+#       subset[position] = candidate
+#       backtrack(subset, input, result, position + 1)
 #     end
 #   end
 #   result
 # end
 class AlgorithmDesign
   def backtrack(input)
-    partial = []
+    subset = []
     result = []
 
     dfs = lambda do |position|
       if a_solution?(position, input)
-        result << process_solution(partial, input)
-      else
-        construct_candidates.each do |candidate|
-          partial[position] = candidate
-          dfs.call(position + 1)
-        end
+        result << process_solution(subset, input)
+        return result
+      end
+
+      construct_candidates.each do |candidate|
+        subset[position] = candidate
+        dfs.call(position + 1)
       end
     end
 
@@ -35,7 +36,7 @@ class AlgorithmDesign
     result
   end
 
-  # Return true if the first k elements of partial form a complete solution
+  # Return true if the first k elements of subset form a complete solution
   # input is for general information needed by the function,
   # For generating all subsets we use it to pass the size of the input set
   def a_solution?(position, input)
@@ -43,19 +44,19 @@ class AlgorithmDesign
   end
 
   # Return the complete set of possible candidates for the kth
-  # position of partial, given the contents at k - 1
+  # position of subset, given the contents at k - 1
   # def construct_candidates(_partial, _position, _input)
   def construct_candidates
     [true, false]
   end
 
   # Print, count, or otherwise process the solution once found
-  def process_solution(partial, input)
-    # partial = [true, true, true]
+  def process_solution(subset, input)
+    # subset = [true, true, true]
     # input = [1, 2, 3]
     # zip = [[true, 1], [true, 2], [true, 3]]
-    # If partial[i] is true, include input[i] in the result, else skip it
-    partial.zip(input).reduce([]) { |result, (p, i)| p ? result << i : result }
+    # If subset[i] is true, include input[i] in the result, else skip it
+    subset.zip(input).reduce([]) { |result, (p, i)| p ? result << i : result }
   end
 end
 
@@ -64,15 +65,15 @@ def subsets(nums)
   dfs([], nums, [])
 end
 
-def dfs(partial, input, result, position = 0)
-  if partial.length == input.length
-    result << partial.zip(input).reduce([]) do |result, (p, i)|
+def dfs(subset, input, result, position = 0)
+  if subset.length == input.length
+    result << subset.zip(input).reduce([]) do |result, (p, i)|
       p ? result << i : result
     end
   else
     [true, false].each do |candidate|
-      partial[position] = candidate
-      backtrack(partial, input, result, position + 1)
+      subset[position] = candidate
+      backtrack(subset, input, result, position + 1)
     end
   end
   result
