1b986a47ae082c48d258e444da5cc8120e00fa8c
--show-origin <divoapple@gmail.com>
2024-09-26 09:26:05 +0100

    703. kth largest using simple sorting
    Timesout for exec, needs a heap

--- interview_prep/code/703.kth-largest-element-in-a-stream.rb
+++ interview_prep/code/703.kth-largest-element-in-a-stream.rb
@@ -1,24 +1,19 @@
 # Implement a heap
 # Need a method to bubble up inserted elements
 
-require 'rb_heap"'
 class KthLargest
   def initialize(k, nums)
     @k = k
-    @heap = Heap.new
-    nums.each { |n| @heap.add(n) }
-    @heap.pop until @heap.size <= @k
+    @nums = nums
   end
 
   # Return kth largest element
   def add(val)
-    @heap.add(val)
-    @heap.pop until @heap.size <= k
-    @heap.peak
+    @nums << val
+    @nums.sort!.reverse[@k - 1]
   end
 end
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest.new(k, nums)
 # param_1 = obj.add(val)
-
