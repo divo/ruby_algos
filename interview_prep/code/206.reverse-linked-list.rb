b5494b1ac05acf24e91165cf61ffe2bb10fa097d
divo <divoapple@gmail.com>
2024-09-29 09:03:35 +0000

    206. Reverse linked list
    Cleaning up a bit, playing around with Ruby

--- interview_prep/code/206.reverse-linked-list.rb
+++ interview_prep/code/206.reverse-linked-list.rb
@@ -1,11 +1,19 @@
 # Definition for singly-linked list.
-# class ListNode
-#   attr_accessor :val, :next
-#   def initialize(val = 0, _next = nil)
-#     @val = val
-#     @next = _next
-#   end
-# end
+ class ListNode
+   attr_accessor :val, :next
+   def initialize(val = 0, _next = nil)
+     @val = val
+     @next = _next
+   end
+
+   def inspect
+     "#{@val} -> #{@next || "nil"}"
+   end
+
+   def to_s
+     inspect
+   end
+ end
 # @param {ListNode} head
 # @return {ListNode}
 # 3 -> 4 -> 5
@@ -15,18 +23,20 @@ end
 
 def do_reverse(head, prev)
   return prev unless head
-  n_node = head.next
-  head.next = prev
+
+  n_node, head.next = head.next, prev
   do_reverse(n_node, head)
 end
 
 def reverse_list_iterative(head)
   prev = nil
   while head
-    n_node = head.next
-    head.next = prev
-    prev = head
-    head = n_node
+    head.next, prev, head = prev, head, head.next
   end
   prev
 end
+
+list = ListNode.new(1, ListNode.new(2, ListNode.new(3)))
+puts list
+puts(list = reverse_list_iterative(list))
+puts(reverse_list_iterative(list))
