72ff65bf21f3af12bb7c2d478ba2dea3b4b81e00
--show-origin <divoapple@gmail.com>
2024-11-11 11:06:09 +0000

    Climbing stairs, brute force
    Pretty sure this is correct, but leetcode just went down

--- interview_prep/code/70.climbing-stairs.rb
+++ interview_prep/code/70.climbing-stairs.rb
@@ -1,4 +1,21 @@
 # @param {Integer} n
 # @return {Integer}
 def climb_stairs(n)
+  result = []
+
+  dfs = lambda { |current|
+    return if current.sum > n
+
+    if current.sum == n
+      result << current
+      return
+    end
+
+    dfs.call(current + [1])
+    dfs.call(current + [2])
+  }
+
+  dfs.call([])
+
+  result.length
 end
