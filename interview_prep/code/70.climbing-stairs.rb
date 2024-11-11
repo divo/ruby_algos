847d07ad65add498753d82fca96c44645d49ec9a
--show-origin <divoapple@gmail.com>
2024-11-11 14:51:42 +0000

    Climbing stairs DP solution

--- interview_prep/code/70.climbing-stairs.rb
+++ interview_prep/code/70.climbing-stairs.rb
@@ -1,21 +1,28 @@
 # @param {Integer} n
 # @return {Integer}
 def climb_stairs(n)
-  result = []
+  one = 1
+  two = 1
+  (n - 1).times { one, two = one + two, one }
+  one
+end
+
+def climb_stairs_bf(n)
+  result = 0
 
   dfs = lambda { |current|
-    return if current.sum > n
+    return if current > n
 
-    if current.sum == n
-      result << current
+    if current == n
+      result += 1
       return
     end
 
-    dfs.call(current + [1])
-    dfs.call(current + [2])
+    dfs.call(current + 1)
+    dfs.call(current + 2)
   }
 
-  dfs.call([])
+  dfs.call(0)
 
-  result.length
+  result
 end
