3f6a4a2487943c103bdd5f21f83047dd6e657df3
--show-origin <divoapple@gmail.com>
2024-11-26 11:59:08 +0000

    Panagram, not much improvement. Will cover sets later

--- exercism/python/pangram/pangram.py
+++ exercism/python/pangram/pangram.py
@@ -1,6 +1,7 @@
 from pdb import set_trace as bp
 
 
+# The better way is with a set, which I will come to later
 def is_pangram(sentence):
     counts = [0] * 26
 
@@ -9,8 +10,4 @@ def is_pangram(sentence):
         if index >= 0 and index <= 26:
             counts[ord(c) - 97] = 1
 
-    sum = 0
-    for n in counts:
-        sum += n
-
-    return sum == 26
+    return sum(counts) == 26
