1f38ce1840e71e9b81353c3c390c76b01fdda3c9
--show-origin <divoapple@gmail.com>
2025-02-27 10:23:45 +0000

    "Improve" merge intervals

--- interview_prep/code/56.merge-intervals.rb
+++ interview_prep/code/56.merge-intervals.rb
@@ -2,40 +2,35 @@
 # @return {Integer[][]}
 # Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 # Output: [[1,6],[8,10],[15,18]]
+# @param {Integer[][]} intervals
+# @return {Integer[][]}
+# 1. Detect overlapping intervals
+# 2. Merge overlapping intervals
+# [[1,3],[2,6],[8,10],[15,18]]
+# [[1,6],[8,10],[15,18]]
 def merge(intervals)
-  merge_all_intervals(intervals.sort, nil, nil)
-end
+  intervals.sort!
 
-def merge_all_intervals(intervals, rindex = nil, lindex = nil)
-  merge_interval(intervals, rindex, lindex) if rindex
-
-  intervals[...intervals.length - 1].each_with_index do |range, index|
-    n_interval = intervals[index + 1].to_r
-    c_interval = range.to_r
-    if n_interval.overlap(c_interval)
-      return merge_all_intervals(intervals, index, index + 1)
+  left = 0
+  right = 1
+  while right < intervals.length
+    if overlap?(intervals[left], intervals[right])
+      intervals = merge_interval(intervals, left, right)
+    else
+      left += 1
+      right += 1
     end
   end
 
-  return intervals
-end
-
-def merge_interval(intervals, rindex, lindex)
-  right, left = intervals[rindex], intervals[lindex]
-  intervals[rindex] = [[right.first, left.first].min, [right.last, left.last].max]
-  intervals.delete_at(lindex)
   intervals
 end
 
-class Array
-  def to_r
-    return (self.first..self.last)
-  end
+def merge_interval(intervals, l, r)
+  intervals[l] = [intervals[l].first, [intervals[l].last, intervals[r].last].max]
+  intervals.delete_at(r)
+  intervals
 end
 
-class Range
-  def overlap(other)
-    self.include?(other.first) || self.include?(other.last) ||
-    other.include?(self.first) || other.include?(self.last)
-  end
+def overlap?(a, b)
+  a.last >= b.first
 end
