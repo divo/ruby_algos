e8cf0cec5486bbed155165e7baefa670809897b7
--show-origin <divoapple@gmail.com>
2025-02-28 10:40:45 +0000

    Valid palindrome without regex

--- interview_prep/code/125.valid-palindrome.rb
+++ interview_prep/code/125.valid-palindrome.rb
@@ -1,8 +1,33 @@
 # @param {String} s
 # @return {Boolean}
+# @param {String} s
+# @return {Boolean}
+def is_palindrome(s)
+  return true if s.length <= 1
+
+  valid_chars = ('a'..'z').to_a + (0..9).map(&:to_s)
+  chars = s.chars
+  l = 0
+  r = chars.length - 1
+  while true
+    # Skip punctuation
+    l += 1 until valid_chars.include?(chars[l]&.downcase) || l > chars.length
+    r -= 1 until valid_chars.include?(chars[r]&.downcase) || r < 0
+
+    return true if l >= r
+
+    return false unless chars[l].downcase == chars[r].downcase
+
+    l += 1
+    r -= 1
+  end
+  true
+end
+
 def is_palindrome(s)
   chars = s.downcase.gsub(/[[:punct:]\s]/, '')
-  left, right = 0, chars.length - 1
+  left = 0
+  right = chars.length - 1
 
   while left < right
     return false unless chars[left] == chars[right]
