142c0828a8e61702d77a8abd1fe9f2cff0fb8ca1
--show-origin <divoapple@gmail.com>
2025-03-14 12:44:45 +0000

    133 Clone graph
    I got it this time, but my solution is pretty ugly

--- interview_prep/code/133.clone-graph.rb
+++ interview_prep/code/133.clone-graph.rb
@@ -28,3 +28,48 @@ def df_clone(node, clones)
   new_node
 end
 
+# Definition for a Node.
+# class Node
+#     attr_accessor :val, :neighbors
+#     def initialize(val = 0, neighbors = nil)
+#		  @val = val
+#		  neighbors = [] if neighbors.nil?
+#         @neighbors = neighbors
+#     end
+# end
+
+# @param {Node} node
+# @return {Node}
+def cloneGraph(node)
+  # Build a list of clones & neighbors
+  return unless node
+
+  clones = []
+  neighbors = []
+  queue = [node]
+
+  until queue.empty?
+    current = queue.shift
+    next if clones[current.val - 1]
+
+    clones[current.val - 1] = Node.new(current.val)
+
+    current.neighbors.each do |n|
+      queue.unshift(n)
+      (neighbors[current.val - 1] ||= []) << n.val
+    end
+  end
+
+  # Walk the neighbors re-linking the clones
+  neighbors.each_with_index do |neighbor_row, parent_index|
+    clones[parent_index].neighbors = neighbor_row.map { |neighbor_index| clones[neighbor_index - 1] }
+  end
+
+  clones.first
+end
+
+class Node
+  def inspect
+    "val : #{val} -> #{neighbors.map(&:val)}"
+  end
+end
