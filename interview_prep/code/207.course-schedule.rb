2d148a0482e6733336c8e6c9277b8bd1d03db61e
--show-origin <divoapple@gmail.com>
2025-03-18 11:35:15 +0000

    207, Kinda shitty throw / catch solution
    I had to lookup my notes (cycle detection is done with DFS, if a node is
    discovered but not processed when you find it, then its an ancestor and
    thus a cycle)
    catch / throw is the easiest way I could think to exit out with the
    result, although I had to lookup how to use it

--- interview_prep/code/207.course-schedule.rb
+++ interview_prep/code/207.course-schedule.rb
@@ -4,6 +4,57 @@
 # @param {Integer} num_courses
 # @param {Integer[][]} prerequisites
 # @return {Boolean}
+
+# @param {Integer} num_courses
+# @param {Integer[][]} prerequisites
+# @return {Boolean}
+def can_finish(num_courses, prerequisites)
+  # DFS the pre-prequisites and stop if we encounter any nodes that are discovered but not processed
+  # First construct an adjacency list we can process
+  adjacent = prerequisites.each_with_object(Hash.new { |h, k| h[k] = [] }) { |(a, b), result| result[a] << b }
+  pp adjacent
+
+  # DFS the adjacency list
+  result = catch(:has_cycle) do
+    num_courses.times do |n|
+      has_cycle?(n, adjacent)
+    end
+    false
+  end
+
+  !result
+end
+
+def has_cycle?(root, graph, processed = [], discovered = [])
+  throw :has_cycle, true if discovered[root] && !processed[root]
+
+  discovered[root] = true
+  graph[root].each do |adjacent|
+    has_cycle?(adjacent, graph, processed, discovered)
+  end
+
+  processed[root] = true
+  false
+end
+
+def can_finish_queue(_num_courses, prerequisites)
+  prereq_map = prerequisites.each_with_object(Hash.new { |h, k| h[k] = [] }) { |(a, b), result| result[a] << b }
+  pp prereq_map
+  # Search each entry in prereq_map for self
+  prereq_map.keys.each do |key|
+    queue = prereq_map[key]
+    until queue.empty?
+      current = queue.shift
+      puts "map #{prereq_map[current]}"
+      return false if prereq_map[current].include?(key)
+
+      queue << prereq_map[current] unless prereq_map[current].empty?
+    end
+  end
+
+  true
+end
+
 def can_finish(_num_courses, prerequisites)
   prereq_map = prerequisites.each_with_object(Hash.new { |h, k| h[k] = [] }) { |(a, b), result| result[a] << b }
   pp prereq_map
