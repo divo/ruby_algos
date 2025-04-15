985f0927571d6a078feee0e2d66a63972bbe24ed
--show-origin <divoapple@gmail.com>
2025-04-15 17:48:57 +0100

    743, Incredibly bad effort.
    I had to diff with neetcode code, too frustrated to actually think
    and just smashing at it in anger
    Fuck I hate this crap

--- interview_prep/code/743.network-delay-time.rb
+++ interview_prep/code/743.network-delay-time.rb
@@ -1,4 +1,44 @@
+# Want to find the shortest path between all nodes
+# That means we need to use Dijkstras, a BFS algo
+# Build the graph, where ex. {2} = [[1, 1], [3, 1]], node = [dest, cost] pairs
+# Start at k and BFS search all nodes using a heap to find the next cheapest path
+# Stop when we have visited all the nodes
 def network_delay_time(times, n, k)
+  graph = times.each_with_object(Hash.new { |h, k| h[k] = [] }) do |(source, dest, cost), result|
+    # Example graph 2 = [[1, 1], [3, 1]]
+    result[source] << [dest, cost]
+  end
+
+  distances = [1_000] * (n + 1)
+  distances[k] = 0
+  visited = []
+  heap = Containers::MinHeap.new
+  heap.push(0, k)
+  total = 0
+
+  until heap.empty?
+    total_cost = heap.next_key
+    node = heap.pop
+    next if visited.include?(node)
+
+    visited << node
+    total = [total, total_cost].max
+
+    graph[node].each do |dest, cost|
+      new_cost = cost + total_cost
+      if new_cost < distances[dest]
+        distances[dest] = new_cost
+        heap.push(new_cost, dest)
+      end
+    end
+  end
+
+  return total if visited.length == n
+
+  -1
+end
+
+def network_delay_time_failed(times, n, k)
   @graph = times.each_with_object(Hash.new { |h, k| h[k] = [] }) do |(source, dest, cost), result|
     # Example graph 2 = [[1, 1], [3, 1]]
     result[source] << [dest, cost]
