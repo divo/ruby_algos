60e0a57db66210faf1d9feccf58e13a825099e51
--show-origin <divoapple@gmail.com>
2025-05-26 16:46:55 +0100

    2131, tweaks. Still timesout. Don't care

--- interview_prep/code/2131.longest-palindrome-by-concatenating-two-letter-words.rb
+++ interview_prep/code/2131.longest-palindrome-by-concatenating-two-letter-words.rb
@@ -2,19 +2,19 @@
 # @return {Integer}
 # ["lc","cl","gg"]
 def longest_palindrome(words)
-  visited = Array.new(words.length)
+  visited = Array.new(words.length, false)
   result = 0
   words.each_with_index do |val_i, idx_i|
-    #words[(idx_i + 1)...].each_with_index do |val_j, idx_j|
-    #  next if visited[idx_i] || visited[idx_j + idx_i]
+    next if visited[idx_i]
 
     words[idx_i.next..].each_with_index do |val_j, idx_j|
-      next if visited[idx_i] || visited[idx_i.next + idx_j]
+      next if visited[idx_i.next + idx_j]
 
       if val_i == val_j.reverse
         result += 4
         visited[idx_i] = true
         visited[idx_j + idx_i.next] = true
+        break
       end
     end
   end
